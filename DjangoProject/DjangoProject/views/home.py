d7e5a42870eab2363a6ac8adbf3990cd2d618443
Said Acosta <saidacostacepeda03@gmail.com>
2025-06-10 23:38:26 -0500

    feat: added content for all variables and statistics

--- DjangoProject/DjangoProject/views/home.py
+++ DjangoProject/DjangoProject/views/home.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import statistics
 from django.shortcuts import render
 from django.conf import settings
 
@@ -27,9 +28,34 @@ def home(request):
     print(f"Ruta CSV: {rute_csv}")
     print(f"Columnas: {column}, Filas: {len(data)}")
 
+    # Variables cuantitativas
+    cuantitativas = [
+        "hours_coding",
+        "coffee_intake_mg",
+        "sleep_hours",
+        "commits",
+        "bugs_reported",
+        "ai_usage_hours",
+        "cognitive_load"
+    ]
+
+    estadisticas = []
+    for col in cuantitativas:
+        if col in df.columns:
+            valores = df[col].dropna()
+            media = valores.mean()
+            mediana = valores.median()
+            try:
+                moda = statistics.mode(valores)
+            except:
+                moda = "Sin moda"
+            desv = valores.std()
+            estadisticas.append([col, round(media, 2), round(mediana, 2), moda, round(desv, 2)])
+
     return render(request, 'charge_csv.html', {
         'Columns': column,
         'Data': data,
         'graphic_data': graphic_data,
+        'estadisticas': estadisticas,
     })
 
